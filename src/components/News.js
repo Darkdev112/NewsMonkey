8877f56c0cdb1d672a1a35c262f7d88a1df040a3
Darkdev112 <devashritcode@gmail.com>
2022-07-11 21:38:37 +0530

    commit made

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
     static defaultProps = {
@@ -22,7 +22,7 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false,
+            loading: true,
             page: 1,
             totalResults: 0
         };
@@ -43,52 +43,59 @@ export class News extends Component {
     }
 
     captalize = (str) => {
-        for(let i=0; i<str.length; i++){
-            return str.charAt(i).toUpperCase() + str.slice(1,str.length);
+        for (let i = 0; i < str.length; i++) {
+            return str.charAt(i).toUpperCase() + str.slice(1, str.length);
         }
     }
-    
+
     componentDidMount() {
         this.updateNews();
         document.title = `NewsMonkey - ${this.captalize(this.props.category)}`;
     }
 
-    handlePreviousButton = () => {
-        this.setState({
-            page: this.state.page - 1
-        });
-        setTimeout(() => {
-            this.updateNews();
-        }, 0);
-    }
-
-    handleNextButton = () => {
+    fetchMoreData = () => {
         this.setState({
             page: this.state.page + 1
         });
-        setTimeout(() => {
-            this.updateNews();
-        }, 0);
+        setTimeout(async() => {
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9ea81e445fd4c55b6c55400987de2c3&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+            this.setState({
+                loading: true
+            })
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: this.state.articles.concat(parsedData.articles),
+                totalResults: parsedData.totalResults,
+                loading: false
+            });
+        }, 0)
     }
+
 
 
     render() {
         return (
-            <div className="container my-3">
+            <>
                 <h1 className="text-center" style={{ margin: "30px 0px" }}>NewsMonkey - Top {this.captalize(this.props.category)} HeadLines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div key={element.url} className="col-md-4 d-flex justify-content-center">
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://st4.depositphotos.com/17828278/24401/v/600/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container my-3">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div key={element.url} className="col-md-4 d-flex justify-content-center">
+                                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://st4.depositphotos.com/17828278/24401/v/600/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                </div>
-                <div className="container d-flex justify-content-between" style={{ margin: "10px 0px" }}>
-                    <button type="button" className="btn btn-dark" onClick={this.handlePreviousButton} disabled={this.state.page <= 1}>	&larr; Previous </button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextButton} disabled={(this.state.page + 1) > Math.ceil(this.state.totalResults / this.props.pageSize)}> Next &rarr; </button>
-                </div>
-            </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
